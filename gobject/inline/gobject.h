1c3bab9550ad3ddf59c74e1bd07ee99fbdc27da9
Alec Davis <unlikelytitan@gmail.com>
2026-04-29 19:49:12 -0700

    added support for easy cleanup of  gobjects

--- gobject/inline/gobject.h
+++ gobject/inline/gobject.h
@@ -1,7 +1,13 @@
 #include <glib-object.h>
+#include <stdint.h>
 
-
-static inline void kk_object_unref(kk_gobject_gobject__gobject object, kk_context_t* ctx) {
-    g_object_unref((gpointer)object.pointer.ptr);
+void gobject_free(void* p, kk_block_t* box, kk_context_t* ctx) {
+    g_object_unref((gpointer)p);
 }
 
+static inline struct kk_gobject_gobject_Gobject kk_gobject_from(kk_box_t ptr, kk_context_t* ctx) {
+    void* raw = kk_cptr_unbox_borrowed(ptr, ctx);
+    kk_box_t box = kk_cptr_raw_box((kk_free_fun_t*)gobject_free, raw, ctx);
+    struct kk_gobject_gobject_Gobject out = { .pointer = (intptr_t)box.box };
+    return out;
+}
